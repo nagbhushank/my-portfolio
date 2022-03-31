1cde611826e72ae73dacb6598b2a1364687bd7e2
2sagarraut2 <https://github.com/2sagarraut2/helloworld.git>
2022-03-31 17:20:09 +0530

    whatsapp code commented

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -49,9 +49,9 @@ export default class Profile extends React.Component {
             <ExternalLink href="https://github.com/2sagarraut2">
               <i className="fab fa-github"></i>
             </ExternalLink>
-            <ExternalLink href="https://api.whatsapp.com/send?phone=+919604848364&text=Hello!%20I%20will%20like%20to%20connect%20to%20with%20you!.">
+            {/* <ExternalLink href="https://api.whatsapp.com/send?phone=+91&text=Hello!%20I%20will%20like%20to%20connect%20to%20with%20you!.">
               <i className="fab fa-whatsapp"></i>
-            </ExternalLink>
+            </ExternalLink> */}
           </span>
         </Figure.Caption>
       </Figure>
