d6c69a4ba1377088e701bca4b2ff522d2d44843e
Sagar Raut <ncs_Sagar.S@nfcguest.com>
2024-09-25 14:31:46 +0530

    skills updated

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,45 +2,51 @@ import React from "react";
 import "./Skills.css";
 import { Container, Image } from "react-bootstrap";
 import C from "../Assets/c-programming.png";
-import sql from "../Assets/Database-mysql.svg.png"
+import sql from "../Assets/Database-mysql.svg.png";
+import spring from "../Assets/spring.png"
 
 export default function Skills() {
   const sikllsOptions = [
     {
       key: 1,
-      value: "C",
-      image: <Image src={C} loading="lazy" />,
-    },
-    {
-      key: 2,
-      value: "HTML",
-      image: <i className="fab fa-html5"></i>,
-    },
-    {
-      key: 3,
-      value: "CSS",
-      image: <i className="fab fa-css3-alt"></i>,
-    },
-    {
-      key: 4,
-      value: "JavaScript",
-      image: <i className="fab fa-js"></i>,
-    },
-    {
-      key: 5,
-      value: "Java",
-      image: <i className="fab fa-java"></i>,
-    },
-    {
-      key: 6,
       value: "ReactJS",
       image: <i className="fab fa-react"></i>,
     },
     {
-      key: 7,
+      key: 2,
+      value: "JavaScript",
+      image: <i className="fab fa-js"></i>,
+    },
+    {
+      key: 3,
+      value: "Java",
+      image: <i className="fab fa-java"></i>,
+    },
+    {
+      key: 4,
+      value: "Spring Boot",
+      image: <Image src={spring} loading="lazy" />,
+    },
+    {
+      key: 5,
       value: "SQL",
       image: <Image src={sql} loading="lazy" />,
-    }
+    },
+    {
+      key: 6,
+      value: "C",
+      image: <Image src={C} loading="lazy" />,
+    },
+    {
+      key: 7,
+      value: "HTML",
+      image: <i className="fab fa-html5"></i>,
+    },
+    {
+      key: 8,
+      value: "CSS",
+      image: <i className="fab fa-css3-alt"></i>,
+    },
   ];
 
   return (
